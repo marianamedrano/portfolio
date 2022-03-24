aed67f0555d81cf619f67e06df9ab580e96104b2
Mariana de Cássia Medrano <maremedrano@yahoo.com>
2022-03-24 00:49:58 -0300

    Portfólio

---
+++ index.js
@@ -0,0 +1,85 @@
+const menu = document.querySelector('nav');
+const descwehug = document.querySelector('.wehug')
+const descnipell = document.querySelector('.nipell')
+const descmcondominio = document.querySelector('.mcondominio')
+const descblog = document.querySelector('.blogpessoal')
+const desceconotrip = document.querySelector('.econotrip')
+const desceduca = document.querySelector('.educa')
+const descmaggu = document.querySelector('.maggu')
+const descportfolio = document.querySelector('.portfolio')
+
+function changedisplay(){
+    menu.classList.toggle('nav')
+}
+
+function mostrarwehug() {
+    if(descwehug.style.display == 'none'){
+        descwehug.style.display = 'flex';
+    }
+    else{
+        descwehug.style.display = 'none';
+    }
+} 
+
+function mostrarnipell() {
+    if(descnipell.style.display == 'none'){
+        descnipell.style.display = 'flex';
+    }
+    else{
+        descnipell.style.display = 'none';
+    }
+} 
+
+function mostrarmcondominio() {
+    if(descmcondominio.style.display == 'none'){
+        descmcondominio.style.display = 'flex';
+    }
+    else{
+        descmcondominio.style.display = 'none';
+    }
+} 
+
+function mostrarblog() {
+    if(descblog.style.display == 'none'){
+        descblog.style.display = 'flex';
+    }
+    else{
+        descblog.style.display = 'none';
+    }
+} 
+
+function mostrareconotrip() {
+    if(desceconotrip.style.display == 'none'){
+        desceconotrip.style.display = 'flex';
+    }
+    else{
+        desceconotrip.style.display = 'none';
+    }
+} 
+
+function mostrareduca() {
+    if(desceduca.style.display == 'none'){
+        desceduca.style.display = 'flex';
+    }
+    else{
+        desceduca.style.display = 'none';
+    }
+} 
+
+function mostrarmaggu() {
+    if(descmaggu.style.display == 'none'){
+        descmaggu.style.display = 'flex';
+    }
+    else{
+        descmaggu.style.display = 'none';
+    }
+} 
+
+function mostrarportfolio() {
+    if(descportfolio.style.display == 'none'){
+        descportfolio.style.display = 'flex';
+    }
+    else{
+        descportfolio.style.display = 'none';
+    }
+} 
